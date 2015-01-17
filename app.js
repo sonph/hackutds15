39342c28315705c1a77c11aa87f9bda27f08c83a
Son Pham <sp@sonpham.me>
2015-01-17 16:06:06 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,92 @@
+'use strict';
+
+var app = angular.module('app', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'home.html',
+    controller: 'HomeCtrl'
+  })
+  .when('/about', {
+    templateUrl: 'about.html',
+    controller: 'AboutCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+  // $locationProvider.html5Mode(true);
+}]);
+
+app.run(function($rootScope) {
+  $(document).ready(function() {
+    $('.dropdown-button').dropdown({"hover": false});
+    // if (window_width > 600) {
+    //   $('ul.tabs').tabs();
+    // }
+    // else {
+    //   $('ul.tabs').hide();
+    // }
+    $('.tab-demo').show().tabs();
+    $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+    $('.tooltipped').tooltip({"delay": 45});
+    $('.collapsible-accordion').collapsible();
+    $('.collapsible-expandable').collapsible({"accordion": false});
+    $('.materialboxed').materialbox();
+    $('.scrollspy').scrollSpy();
+    $('.button-collapse').sideNav();
+  });
+});
+
+app.controller('HomeCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+  $rootScope.root = {
+    title: 'Home'
+  };
+
+  // Github Latest Commit
+  if ($('#lastUpdated').length) { // Checks if widget div exists (Index only)
+    $.ajax({
+      url: "https://api.github.com/repos/sonph/sonph.github.io/commits/master",
+      dataType: "json", 
+      success: function (data) {
+        var sha = data.sha;
+        var date = jQuery.timeago(data.commit.author.date);
+        // if (window_width < 600) {
+        //   sha = sha.substring(0,7);
+        // }
+        // $('.github-commit').find('.date').html(date);
+        $('#lastUpdated').html(date);
+        //$('.github-commit').find('.sha').html(sha).attr('href', data.html_url);
+        // $('.github-commit').find('.sha').html(sha.substring(0, 7));
+
+        // console.log(returndata, returndata.commit.author.date, returndata.sha);
+      }  
+    });      
+  }
+
+  $('.dropdown-button').dropdown({"hover": false});
+  if (window_width > 600) {
+    $('ul.tabs').tabs();
+  }
+  else {
+    $('ul.tabs').hide();
+  }
+  $('.tab-demo').show().tabs();
+  $('.parallax').parallax();
+  $('.modal-trigger').leanModal();
+  $('.tooltipped').tooltip({"delay": 45});
+  $('.collapsible-accordion').collapsible();
+  $('.collapsible-expandable').collapsible({"accordion": false});
+  $('.materialboxed').materialbox();
+  $('.scrollspy').scrollSpy();
+  $(document).ready(function() {
+    $('.button-collapse').sideNav();
+  });
+}]);
+
+
+app.controller('AboutCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+  $rootScope.root = {
+    title: 'About'
+  };
+}]);
